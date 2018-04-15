594699ba00e7f423b5044c341ef8ba761c2b9b6c
Maxime Baudin <mb.baudin@gmail.com>
2018-04-15 16:25:39 +0200

    Add lopez sound

--- app/src/main/java/com/maxtho/soundboxmaker/homepage/soundtab/data/SoundsDataPump.java
+++ app/src/main/java/com/maxtho/soundboxmaker/homepage/soundtab/data/SoundsDataPump.java
@@ -12,39 +12,39 @@ public class SoundsDataPump {
     public static HashMap<String, List<Sound>> getData() {
         HashMap<String, List<Sound>> expandableListDetail = new HashMap<String, List<Sound>>();
 
-        Sound s1 = new Sound().setName("TVPALC")
+        Sound s1 = new Sound().setName("Poule")
                 .setSoundReference("" + R.raw.poulepondeuse)
                 .setDefault(true);
-        Sound s2 = new Sound().setName("Pssss")
+        Sound s2 = new Sound().setName("Gun")
                 .setSoundReference("" + R.raw.quarantecinq)
                 .setDefault(true);
-        Sound s3 = new Sound().setName("um um")
-                .setSoundReference("" + R.raw.poulepondeuse)
+        Sound s3 = new Sound().setName("Cri")
+                .setSoundReference("" + R.raw.cri)
                 .setDefault(true);
-        Sound s4 = new Sound().setName("Ui")
-                .setSoundReference("" + R.raw.quarantecinq)
+        Sound s4 = new Sound().setName("Hey Hofman")
+                .setSoundReference("" + R.raw.hehofman)
                 .setDefault(true);
-        Sound s5 = new Sound().setName("a")
-                .setSoundReference("" + R.raw.quarantecinq)
+        Sound s5 = new Sound().setName("Je vais vous...")
+                .setSoundReference("" + R.raw.jvaisvousenculer)
                 .setDefault(true);
-        Sound s6 = new Sound().setName("b")
-                .setSoundReference("" + R.raw.poulepondeuse)
+        Sound s6 = new Sound().setName("La calotte")
+                .setSoundReference("" + R.raw.lacalote)
                 .setDefault(true);
-        Sound s7 = new Sound().setName("c")
-                .setSoundReference("" + R.raw.quarantecinq)
+        Sound s7 = new Sound().setName("Lopez de vos morts")
+                .setSoundReference("" + R.raw.lopezdevosmorts)
                 .setDefault(true);
-        Sound s8 = new Sound().setName("d")
-                .setSoundReference("" + R.raw.poulepondeuse)
+        Sound s8 = new Sound().setName("Ta femme")
+                .setSoundReference("" + R.raw.tafemme)
                 .setDefault(true);
-        Sound s9 = new Sound().setName("e")
-                .setSoundReference("" + R.raw.quarantecinq)
+        Sound s9 = new Sound().setName("Viens la toi")
+                .setSoundReference("" + R.raw.vienslatoi)
                 .setDefault(true);
-        Sound s10 = new Sound().setName("f")
-                .setSoundReference("" + R.raw.poulepondeuse)
+        Sound s10 = new Sound().setName("Viens on va en finir")
+                .setSoundReference("" + R.raw.viensonvaenfinir)
                 .setDefault(true);
-        expandableListDetail.put("DxP", Arrays.asList(s1, s2, s3, s4));
-        expandableListDetail.put("Repliques", Arrays.asList(s5, s6, s7, s8, s9, s10));
-        expandableListDetail.put("Insultes", Arrays.asList(s1, s2, s3, s4));
+        expandableListDetail.put("Animaux", Arrays.asList(s1));
+        expandableListDetail.put("Armes", Arrays.asList(s2));
+        expandableListDetail.put("Lopez", Arrays.asList(s3, s4, s5, s6, s7, s8, s9));
         return expandableListDetail;
     }
 }
