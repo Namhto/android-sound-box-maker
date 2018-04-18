0b4d6e0533edbbb2ea74ad2ef4764051b0a807c4
Maxime Baudin <mb.baudin@gmail.com>
2018-04-18 21:14:43 +0200

    [SplashScreen] Add basic splashscreen

---
+++ app/src/main/java/com/maxtho/soundboxmaker/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.maxtho.soundboxmaker.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.maxtho.soundboxmaker.Injector;
+import com.maxtho.soundboxmaker.homepage.HomePageActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Injector.getInstance().getAppComponent().inject(this);
+
+        //TODO Remove Handler for prod app
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, HomePageActivity.class));
+                finish();
+            }
+        }, 1000);
+
+
+    }
+}
